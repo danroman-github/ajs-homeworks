ca1d674ab1dd3be6c21ce4395c2574e2d41d4734
Роман Данилов <danroman@rambler.ru>
2025-11-22 15:48:28 +0300

    udate folder library-ajs

---
+++ library-ajs/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'ajs',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: {
+                  browsers: ['> 1%', 'last 2 versions']
+                },
+                modules: 'commonjs'
+              }]
+            ]
+          }
+        },
+      },
+    ],
+  },
+};
