9a530a4ac772ee2f7be5dc4b535c6babbb81b380
Роман Данилов <danroman@rambler.ru>
2026-01-02 10:36:12 +0300

    remote folder

--- container-set/src/js/app.js
+++
@@ -1,8 +0,0 @@
-import {func, num} from './one';
-import example from '../../../example.json';
-
-func();
-console.log(num);
-console.log(example);
-const main = document.querySelector('.main');
-main.textContent = 'Я текст из файла app.js';
